8aeacdeb94d1cc5d2c438aed305568f69eb943f8
francis omondi <francisomondi17@gmail.com>
2022-08-23 13:14:47 +0300

    connecting to database

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const connectDb = require("./config/db")
 
 //load config
 dotenv.config({
     path: "./config/config.env"
 })
 
+
+connectDb()
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, console.log(`Server started on ${process.env.NODE_ENV} mode on port ${PORT}`))
