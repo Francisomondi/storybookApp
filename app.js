bcd12b34941672111b1c7c2d7771ae18b6a70a39
francis omondi <francisomondi17@gmail.com>
2022-08-23 13:37:01 +0300

    middleware and view engine

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ const express = require("express")
 const dotenv = require("dotenv")
 const connectDb = require("./config/db")
 const morgan = require("morgan")
+const ejs = require("ejs")
 
 //load config
 dotenv.config({
     path: "./config/config.env"
 })
-
-
 connectDb()
+
 const app = express()
 
 //logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"))
 }
+//middleware/static files
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+//view engine
+app.set("view engine", "ejs")
 
 const PORT = process.env.PORT || 3000
 
