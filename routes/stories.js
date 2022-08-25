5494007e1ffd685bcd3c047b3c4aeae176f73a60
francis omondi <francisomondi17@gmail.com>
2022-08-25 22:36:02 +0300

    display stories to our dashboard

--- routes/stories.js
+++ routes/stories.js
@@ -10,6 +10,25 @@ router.get("/add", (req, res) => {
     res.render("stories/add", ensureAuth)
 })
 
+//@description show add page
+//@route POST /stories
+router.post("/", async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await story.create(req.body)
+        res.redirect("/dashboard")
+
+    } catch (err) {
+        console.log(err)
+        res.render("stories/500")
+    }
+
+
+})
+
+
+
+
 
 
 module.exports = router
